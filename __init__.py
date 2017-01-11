7ff4062532cbf552307ede1d5a18dd0a9bfab27d
Raimon Esteve <raimon@nan-tic.com>
2017-01-11 14:41:05 +0100

    Review some parts before create new version

--- __init__.py
+++ __init__.py
@@ -2,12 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
-from .asset import *
-from .product import *
+from . import asset
+from . import product
 
 
 def register():
     Pool.register(
-        Asset,
-        Template,
+        asset.Asset,
+        product.Template,
         module='account_asset_percentatge', type_='model')
