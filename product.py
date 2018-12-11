7d89e218ae481d999c8b9de324b1e8487d1af6d3
?ngel ?lvarez <angel@nan-tic.com>
2018-12-11 11:51:18 +0100

    change property to multivalue

--- product.py
+++ product.py
@@ -3,7 +3,8 @@
 # the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pyson import Eval
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
+from trytond import backend
 
 __all__ = ['Template']
 
@@ -11,7 +12,7 @@ __all__ = ['Template']
 class Template:
     __metaclass__ = PoolMeta
     __name__ = 'product.template'
-    depreciation_percentatge = fields.Property(fields.Numeric(
+    depreciation_percentatge = fields.MultiValue(fields.Numeric(
             'Depreciation Percentatge', digits=(16, 4),
             states={
                 'readonly': ~Eval('active', True),
@@ -35,3 +36,39 @@ class Template:
         if self.depreciation_duration:
             self.depreciation_percentatge = (
                 12 / self.depreciation_duration)
+
+    @classmethod
+    def multivalue_model(cls, field):
+        pool = Pool()
+        if field in {'depreciation_percentatge'}:
+            return pool.get('product.template.account')
+        return super(Template, cls).multivalue_model(field)
+
+
+class TemplateAccount:
+    __metaclass__ = PoolMeta
+    __name__ = 'product.template.account'
+
+    depreciation_percentatge = fields.Numeric(
+        'Depreciation Percentatge', digits=(16, 4),
+        states={
+            'readonly': ~Eval('active', True),
+            'invisible': (~Eval('depreciable')
+                | (Eval('type', '') != 'assets')
+                | ~Eval('context', {}).get('company')),
+            },
+        depends=['active', 'depreciable', 'type'])
+
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        exist = TableHandler.table_exist(cls._table)
+        if exist:
+            table = TableHandler(cls, module_name)
+            exist &= (table.column_exist('depreciation_percentatge'))
+
+        super(TemplateAccount, cls).__register__(module_name)
+
+        if not exist:
+            # Re-migration
+            cls._migrate_property([], [], [])
