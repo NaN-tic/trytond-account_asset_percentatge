c0334857fb7c8b86d0a3b5a7c816694dbcaabef6
Raimon Esteve <raimonesteve@gmail.com>
2021-08-24 09:32:18 +0200

    Add monetary field issue10598

--- asset.py
+++ asset.py
@@ -1,15 +1,14 @@
 # This file is part account_asset_percent module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.model import fields
 from trytond.pyson import Eval
 from trytond.pool import PoolMeta
+from trytond.modules.currency.fields import Monetary
 
 
 class Asset(metaclass=PoolMeta):
     __name__ = 'account.asset'
-    purchase_value = fields.Numeric('Purchase Value',
-        digits=(16, Eval('currency_digits', 2)), states={
+    purchase_value = Monetary('Purchase Value',
+        digits='currency', currency='currency', states={
             'readonly': (Eval('lines', [0]) | (Eval('state') != 'draft')),
-            },
-        depends=['currency_digits', 'state'])
+        }, depends=['state'])
